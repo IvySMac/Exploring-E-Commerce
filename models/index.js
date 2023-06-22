75c3231fdecc9f7e1d039db9758d35c527e0da09
Creature Feature <ivy.mccurdy@gmail.com>
2023-06-21 23:42:28 -0500

    edited the .env file and connection file to make sure SQL could communicate with node and that sequilize worked correctly, added foreign keys to index.js file in models folder, set up fields and rules in product and category files in models folder, updated routs

--- models/index.js
+++ models/index.js
@@ -1,22 +1,26 @@
-const Product = require("./Product");
-const Category = require("./Category");
-const Tag = require("./Tag");
-const ProductTag = require("./ProductTag");
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { 
+Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: tag_id
- });
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { 
+Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: product_id 
- });
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
